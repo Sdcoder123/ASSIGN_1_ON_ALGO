64caaceea878fc3f6d7c485bc795acc5e96539de
Sdcoder123 <98347730+Sdcoder123@users.noreply.github.com>
2022-03-01 01:41:31 +0530

    Add files via upload

---
+++ 1B/1b_uniform_distri.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>   //for random seed
+#include <stdlib.h> //for rand() and srand()
+
+const int NO_OF_ELEMENTS = 10e6;
+const int MAX_VALUE = 100;
+const int NO_OF_SUB_ELEMENTS = 10;
+
+void main()
+{
+    srand(time(0));
+
+    FILE *fout = fopen("normal_distribution.csv", "w");
+
+    for (int i = 0; i < NO_OF_ELEMENTS; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < NO_OF_SUB_ELEMENTS; j++)
+        {
+            sum += rand() % (MAX_VALUE / NO_OF_SUB_ELEMENTS);
+        }
+        fprintf(fout, "%d\n", sum);
+    }
+    fclose(fout);
+}
